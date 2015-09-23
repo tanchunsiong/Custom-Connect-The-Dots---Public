649f5959c2cb9b47f8697fecb0255dc2eee4faf6
Tan Chun Siong <dreamtcs@hotmail.com>
2015-09-23 08:50:54 +0800

    added alert for eventhub nodejs

--- connectthedots nodejs website with highcharts/index.js
+++ connectthedots nodejs website with highcharts/index.js
@@ -26,6 +26,20 @@ var uriappend = "ConsumerGroups/" + consumerGroup + "/Partitions/";
 var Sbus = require('sbus-amqp10');
 var hub = Sbus.EventHubClient(serviceBus, eventHubName, sasKeyName, sasKey);
 
+
+// Set up variables
+var serviceBus2 = 'cspi1-ns',
+    eventHubName2 = 'ehalerts',
+    sasKeyName2 = 'WebSite', // A SAS Key Name for the Event Hub, with Receive privilege
+    sasKey2 = 'trgtkexrsPgvJZlDTdvsiYQTeMxpPt9DTDkIZfB1lrI=', // The key value
+    consumerGroup2 = 'nodejsconsumergroup',
+    numPartitions2 = 8;
+
+var uriappend2 = "ConsumerGroups/" + consumerGroup + "/Partitions/";
+
+var Sbus2 = require('sbus-amqp10');
+var hub2 = Sbus2.EventHubClient(serviceBus, eventHubName, sasKeyName, sasKey);
+
 wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
         client.send(data);
@@ -41,6 +55,7 @@ wss.on('connection', function connection(ws) {
   
 });
 
+//this is for reading from ehdevices
 for (var idx = 0; idx < numPartitions; ++idx) {
     hub.eventHubReceive(uriappend + idx, new Date().getTime() - new Date().getTimezoneOffset(), function callback(rx_err, partition, payload) {
         if (rx_err) {
@@ -57,6 +72,21 @@ for (var idx = 0; idx < numPartitions; ++idx) {
     );
 }
 
-
+//this is for reading from ehalerts
+for (var idx = 0; idx < numPartitions2; ++idx) {
+    hub2.eventHubReceive(uriappend2 + idx, new Date().getTime() - new Date().getTimezoneOffset(), function callback(rx_err, partition, payload) {
+        if (rx_err) {
+            console.log(rx_err);
+        } else {
+            console.log("partition: " + partition);
+            console.log(payload);
+            
+            var tmpstring = JSON.stringify(payload);
+            tmpstring = tmpstring.replace("}", ",\"time\":\"" + payload.timecreated + "\"}");
+            wss.broadcast(tmpstring);
+        }
+    }
+    );
+}
 
 
