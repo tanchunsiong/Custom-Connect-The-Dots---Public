37abde50c90a8bf43ed5c68cd22d4ac9e87e1f63
Tan Chun Siong <dreamtcs@hotmail.com>
2015-09-26 02:18:17 +0800

    commited

--- arduino sketch/connectthedots - sensors connected arduino/connectthedots/connectthedots.ino
+++ arduino sketch/connectthedots - sensors connected arduino/connectthedots/connectthedots.ino
@@ -4,15 +4,15 @@
 #define dht_dpin A1 //no ; here. Set equal to channel sensor is on
 dht11 DHT;
 
-int ledPinRed =6;
-int ledPinGreen=8;
+int ledPinRed = 6;
+int ledPinGreen = 8;
 
 
 const int LightSensorPin = A0;  // Analog input pin that the potentiometer is attached to
-float lightsensorValue = 0; 
+float lightsensorValue = 0;
 
 const int pirPin = 2;    //the digital pin connected to the PIR sensor's output
-float motionvalue=0;
+float motionvalue = 0;
 
 
 
@@ -39,75 +39,76 @@ long unitId;
 
 
 
-void setup(){
-   Serial.begin(9600);
+void setup() {
+	Serial.begin(9600);
 
-   delay(1000);//Wait rest of 1000ms recommended delay before
-   pinMode(ledPinRed, OUTPUT);
-   pinMode(ledPinGreen, OUTPUT);
-   String guid=generateGuid();
-   guid.toCharArray(GUID1, 37);
-    guid=generateGuid();
-   guid.toCharArray(GUID2, 37);
-    guid=generateGuid();
-   guid.toCharArray(GUID3, 37);
-    guid=generateGuid();
-   guid.toCharArray(GUID4, 37);
-   
+	delay(1000);//Wait rest of 1000ms recommended delay before
+	pinMode(ledPinRed, OUTPUT);
+	pinMode(ledPinGreen, OUTPUT);
+	String guid = generateGuid();
+	guid.toCharArray(GUID1, 37);
+	guid = generateGuid();
+	guid.toCharArray(GUID2, 37);
+	guid = generateGuid();
+	guid.toCharArray(GUID3, 37);
+	guid = generateGuid();
+	guid.toCharArray(GUID4, 37);
+
 }//end "setup()"
 
-String generateGuid(){
-   String firstpart=generate8();
-   while (firstpart.length() <8){
-       firstpart=generate8();
-   }
-   
-   String secondtemppart=generate8();
-   String secondpart=secondtemppart.substring(0,4);
-   String thirdpart=secondtemppart.substring(3,7);
-   
-   String fourthtemppart=generate8();
-   String fourthpart=fourthtemppart.substring(0,4);
-   
-   String fifthtemppart=generate8();
-     while (fifthtemppart.length() <8){
-       fifthtemppart=generate8();
-   }
-    String fifthtemppart2=fourthtemppart.substring(3,7);
-    String fifthpart=fifthtemppart + fifthtemppart2;
-    
+String generateGuid() {
+	String firstpart = generate8();
+	while (firstpart.length() <8) {
+		firstpart = generate8();
+	}
+
+	String secondtemppart = generate8();
+	String secondpart = secondtemppart.substring(0, 4);
+	String thirdpart = secondtemppart.substring(3, 7);
+
+	String fourthtemppart = generate8();
+	String fourthpart = fourthtemppart.substring(0, 4);
+
+	String fifthtemppart = generate8();
+	while (fifthtemppart.length() <8) {
+		fifthtemppart = generate8();
+	}
+	String fifthtemppart2 = fourthtemppart.substring(3, 7);
+	String fifthpart = fifthtemppart + fifthtemppart2;
 
 
-return (firstpart+"-"+secondpart+"-"+thirdpart+"-"+fourthpart+"-"+fifthpart);
+
+	return (firstpart + "-" + secondpart + "-" + thirdpart + "-" + fourthpart + "-" + fifthpart);
 
 }
 
-String generate8(){
- long notunitId;
+String generate8() {
+	long notunitId;
 
 
- unitId = (long)EEPROM.read(0) << 24 | (long)EEPROM.read(1) << 16 | (long)EEPROM.read(2) << 8 | (long)EEPROM.read(3);
-  notunitId = (long)EEPROM.read(4) << 24 | (long)EEPROM.read(5) << 16 | (long)EEPROM.read(6) << 8 | (long)EEPROM.read(7);
+	unitId = (long)EEPROM.read(0) << 24 | (long)EEPROM.read(1) << 16 | (long)EEPROM.read(2) << 8 | (long)EEPROM.read(3);
+	notunitId = (long)EEPROM.read(4) << 24 | (long)EEPROM.read(5) << 16 | (long)EEPROM.read(6) << 8 | (long)EEPROM.read(7);
 
 
-    unitId = notunitId = 4711;
-  if (unitId == -notunitId) {
-    
-    return String(unitId, HEX);
-  } else {
-    randomSeed(analogRead(7)*analogRead(6)*analogRead(5)+micros());
-    unitId = random();
-    notunitId = -unitId;
-    EEPROM.write(0, unitId >> 24 & 0xFF);
-    EEPROM.write(1, unitId >> 16 & 0xFF);
-    EEPROM.write(2, unitId >> 8  & 0xFF);
-    EEPROM.write(3, unitId     & 0xFF);
-    EEPROM.write(4, notunitId >> 24 & 0xFF);
-    EEPROM.write(5, notunitId >> 16 & 0xFF);
-    EEPROM.write(6, notunitId >> 8  & 0xFF);
-    EEPROM.write(7, notunitId     & 0xFF);
-    return String(unitId, HEX);
-  }
+	unitId = notunitId = 4711;
+	if (unitId == -notunitId) {
+
+		return String(unitId, HEX);
+	}
+	else {
+		randomSeed(analogRead(7)*analogRead(6)*analogRead(5) + micros());
+		unitId = random();
+		notunitId = -unitId;
+		EEPROM.write(0, unitId >> 24 & 0xFF);
+		EEPROM.write(1, unitId >> 16 & 0xFF);
+		EEPROM.write(2, unitId >> 8 & 0xFF);
+		EEPROM.write(3, unitId & 0xFF);
+		EEPROM.write(4, notunitId >> 24 & 0xFF);
+		EEPROM.write(5, notunitId >> 16 & 0xFF);
+		EEPROM.write(6, notunitId >> 8 & 0xFF);
+		EEPROM.write(7, notunitId & 0xFF);
+		return String(unitId, HEX);
+	}
 }
 
 
@@ -115,124 +116,128 @@ String generate8(){
 float get_light_level()
 {
 
-  float lightSensor = analogRead(LightSensorPin);
-  lightSensor = 1/lightSensor *1500;
-  return(lightSensor);
+	float lightSensor = analogRead(LightSensorPin);
+	lightSensor = 1 / lightSensor * 1500;
+	return(lightSensor);
 }
 
-void loop(){
-  //This is the "heart" of the program.
-   DHT.read(dht_dpin);
-   lightsensorValue =get_light_level();  
-   motionvalue=digitalRead(pirPin);
+void loop() {
+	//This is the "heart" of the program.
+	DHT.read(dht_dpin);
+	lightsensorValue = get_light_level();
+	motionvalue = digitalRead(pirPin);
 
-  float temperatureC =DHT.temperature;
-float humdity=DHT.humidity;
+	float temperatureC = DHT.temperature;
+	float humdity = DHT.humidity;
 
-       
-  memset(buffer,'\0',sizeof(buffer));
-  strcat(buffer,"{");
-  strcat(buffer,"\"guid\":\"");
-  strcat(buffer,GUID1);
-  strcat(buffer,"\",\"organization\":\"");
-  strcat(buffer,Org);
-  strcat(buffer,"\",\"timecreated\":\"");
-  strcat(buffer,"timenow");
-  strcat(buffer,"\",\"displayname\":\"");
-  strcat(buffer,Disp);
-  strcat(buffer,"\",\"location\":\"");
-  strcat(buffer,Locn);  
-  strcat(buffer,"\",\"measurename\":\"");
-  strcat(buffer,Measure1);
-  strcat(buffer,"\",\"unitofmeasure\":\"");
-  strcat(buffer,Units1);
-  strcat(buffer,"\",\"value\":");
-  strcat(buffer,dtostrf(temperatureC,5,2,dtostrfbuffer));
-  strcat(buffer,"}");
- Serial.println(buffer);
-delay(100);
-  // print string for humidity, separated by line for ease of reading
-  memset(buffer,'\0',sizeof(buffer));
-  strcat(buffer,"{");
-  strcat(buffer,"\"guid\":\"");
-  strcat(buffer,GUID2);
-  strcat(buffer,"\",\"organization\":\"");
-  strcat(buffer,Org);
-  strcat(buffer,"\",\"timecreated\":\"");
-  strcat(buffer,"timenow");
-  strcat(buffer,"\",\"displayname\":\"");
-  strcat(buffer,Disp);
-  strcat(buffer,"\",\"location\":\"");
-  strcat(buffer,Locn);  
-  strcat(buffer,"\",\"measurename\":\"");
-  strcat(buffer,Measure2);
-  strcat(buffer,"\",\"unitofmeasure\":\"");
-  strcat(buffer,Units2);
-  strcat(buffer,"\",\"value\":");
-  strcat(buffer,dtostrf(humdity,5,2,dtostrfbuffer));
-  strcat(buffer,"}");
- Serial.println(buffer);
-delay(100);
-  // print string for light, separated by line for ease of reading
-  memset(buffer,'\0',sizeof(buffer));
-  strcat(buffer,"{");
-  strcat(buffer,"\"guid\":\"");
-  strcat(buffer,GUID3);
-  strcat(buffer,"\",\"organization\":\"");
-  strcat(buffer,Org);
-  strcat(buffer,"\",\"timecreated\":\"");
-  strcat(buffer,"timenow");
-  strcat(buffer,"\",\"displayname\":\"");
-  strcat(buffer,Disp);
-  strcat(buffer,"\",\"location\":\"");
-  strcat(buffer,Locn);  
-  strcat(buffer,"\",\"measurename\":\"");
-  strcat(buffer,Measure3);
-  strcat(buffer,"\",\"unitofmeasure\":\"");
-  strcat(buffer,Units3);
-  strcat(buffer,"\",\"value\":");
-  strcat(buffer,dtostrf(lightsensorValue,5,3,dtostrfbuffer));
-  strcat(buffer,"}");
- Serial.println(buffer);
-delay(100);
- // print string for motion, separated by line for ease of reading
-  memset(buffer,'\0',sizeof(buffer));
-  strcat(buffer,"{");
-  strcat(buffer,"\"guid\":\"");
-  strcat(buffer,GUID4);
-  strcat(buffer,"\",\"organization\":\"");
-  strcat(buffer,Org);
-  strcat(buffer,"\",\"timecreated\":\"");
-  strcat(buffer,"timenow");
-  strcat(buffer,"\",\"displayname\":\"");
-  strcat(buffer,Disp);
-  strcat(buffer,"\",\"location\":\"");
-  strcat(buffer,Locn);  
-  strcat(buffer,"\",\"measurename\":\"");
-  strcat(buffer,Measure4);
-  strcat(buffer,"\",\"unitofmeasure\":\"");
-  strcat(buffer,Units4);
-  strcat(buffer,"\",\"value\":");
-  strcat(buffer,dtostrf(motionvalue,3,1,dtostrfbuffer));
-  strcat(buffer,"}");
- Serial.println(buffer);
- 
- delay(100);
 
- if(Serial.available() > 0)
-    {
-       String str = Serial.readStringUntil('\n');
-    Serial.println(str);
-        if (str == "{command:off}"){
-          digitalWrite(ledPinRed, LOW);
-          digitalWrite(ledPinGreen, LOW);
-        }
-        else if (str == "{command:on}"){
-          digitalWrite(ledPinRed, HIGH);
-          digitalWrite(ledPinGreen, HIGH);
-        }
+	memset(buffer, '\0', sizeof(buffer));
+	strcat(buffer, "{");
+	strcat(buffer, "\"guid\":\"");
+	strcat(buffer, GUID1);
+	strcat(buffer, "\",\"organization\":\"");
+	strcat(buffer, Org);
+	strcat(buffer, "\",\"timecreated\":\"");
+	strcat(buffer, "timenow");
+	strcat(buffer, "\",\"displayname\":\"");
+	strcat(buffer, Disp);
+	strcat(buffer, "\",\"location\":\"");
+	strcat(buffer, Locn);
+	strcat(buffer, "\",\"measurename\":\"");
+	strcat(buffer, Measure1);
+	strcat(buffer, "\",\"unitofmeasure\":\"");
+	strcat(buffer, Units1);
+	strcat(buffer, "\",\"value\":");
+	strcat(buffer, dtostrf(temperatureC, 5, 2, dtostrfbuffer));
+	strcat(buffer, "}");
+	Serial.println(buffer);
+	delay(300);
 
-    }
+	// print string for humidity, separated by line for ease of reading
+	memset(buffer, '\0', sizeof(buffer));
+	strcat(buffer, "{");
+	strcat(buffer, "\"guid\":\"");
+	strcat(buffer, GUID2);
+	strcat(buffer, "\",\"organization\":\"");
+	strcat(buffer, Org);
+	strcat(buffer, "\",\"timecreated\":\"");
+	strcat(buffer, "timenow");
+	strcat(buffer, "\",\"displayname\":\"");
+	strcat(buffer, Disp);
+	strcat(buffer, "\",\"location\":\"");
+	strcat(buffer, Locn);
+	strcat(buffer, "\",\"measurename\":\"");
+	strcat(buffer, Measure2);
+	strcat(buffer, "\",\"unitofmeasure\":\"");
+	strcat(buffer, Units2);
+	strcat(buffer, "\",\"value\":");
+	strcat(buffer, dtostrf(humdity, 5, 2, dtostrfbuffer));
+	strcat(buffer, "}");
+	Serial.println(buffer);
+	delay(300);
+
+	// print string for light, separated by line for ease of reading
+	memset(buffer, '\0', sizeof(buffer));
+	strcat(buffer, "{");
+	strcat(buffer, "\"guid\":\"");
+	strcat(buffer, GUID3);
+	strcat(buffer, "\",\"organization\":\"");
+	strcat(buffer, Org);
+	strcat(buffer, "\",\"timecreated\":\"");
+	strcat(buffer, "timenow");
+	strcat(buffer, "\",\"displayname\":\"");
+	strcat(buffer, Disp);
+	strcat(buffer, "\",\"location\":\"");
+	strcat(buffer, Locn);
+	strcat(buffer, "\",\"measurename\":\"");
+	strcat(buffer, Measure3);
+	strcat(buffer, "\",\"unitofmeasure\":\"");
+	strcat(buffer, Units3);
+	strcat(buffer, "\",\"value\":");
+	strcat(buffer, dtostrf(lightsensorValue, 5, 3, dtostrfbuffer));
+	strcat(buffer, "}");
+	Serial.println(buffer);
+	delay(300);
+
+	// print string for motion, separated by line for ease of reading
+	memset(buffer, '\0', sizeof(buffer));
+	strcat(buffer, "{");
+	strcat(buffer, "\"guid\":\"");
+	strcat(buffer, GUID4);
+	strcat(buffer, "\",\"organization\":\"");
+	strcat(buffer, Org);
+	strcat(buffer, "\",\"timecreated\":\"");
+	strcat(buffer, "timenow");
+	strcat(buffer, "\",\"displayname\":\"");
+	strcat(buffer, Disp);
+	strcat(buffer, "\",\"location\":\"");
+	strcat(buffer, Locn);
+	strcat(buffer, "\",\"measurename\":\"");
+	strcat(buffer, Measure4);
+	strcat(buffer, "\",\"unitofmeasure\":\"");
+	strcat(buffer, Units4);
+	strcat(buffer, "\",\"value\":");
+	strcat(buffer, dtostrf(motionvalue, 3, 1, dtostrfbuffer));
+	strcat(buffer, "}");
+	Serial.println(buffer);
+
+	delay(300);
+
+
+	if (Serial.available() > 0)
+	{
+		String str = Serial.readStringUntil('\n');
+		Serial.println(str);
+		if (str == "{command:off}") {
+			digitalWrite(ledPinRed, LOW);
+			digitalWrite(ledPinGreen, LOW);
+		}
+		else if (str == "{command:on}") {
+			digitalWrite(ledPinRed, HIGH);
+			digitalWrite(ledPinGreen, HIGH);
+		}
+
+	}
 
 }// end loop()
 
--- website - nodejs/server.js
+++ website - nodejs/server.js
@@ -61,12 +61,43 @@ for (var idx = 0; idx < numPartitions; ++idx) {
         if (rx_err) {
             console.log(rx_err);
         } else {
-            console.log("partition: " + partition);
-            console.log(payload);
-            
-            var tmpstring = JSON.stringify(payload);
-            tmpstring = tmpstring.replace("}", ",\"time\":\"" + payload.timecreated + "\"}");
-            wss.broadcast(tmpstring);
+              console.log("partition: " + partition);
+            //console.log(payload);
+		    var tmpstring;
+			//multiline
+			try {
+			
+                tmpstring = JSON.stringify(payload.split('\r\n'));
+				var jsonObj = JSON.parse( tmpstring  );
+		   
+		   	for (var item in jsonObj)
+					{
+					var targetstring =  JSON.stringify(jsonObj[item]);
+				    targetstring = targetstring.replace("}", ",\"time\":\"" + JSON.parse(jsonObj[item]).timecreated + "\"}");
+					console.log("     sending : "+ targetstring);
+                    wss.broadcast(targetstring);
+				}
+			
+			    tmpstring="";
+				
+			
+			}
+			//single item
+			catch(err) {
+				try{
+						tmpstring = JSON.stringify(payload);
+						tmpstring = tmpstring.replace("}", ",\"time\":\"" + payload.timecreated + "\"}");
+						console.log("    sending : "+ tmpstring);
+						wss.broadcast(tmpstring);
+				}catch (err){
+				console.log(err.message);
+				}
+					tmpstring="";
+			}
+
+
+			
+     
         }
     }
     );
@@ -79,11 +110,59 @@ for (var idx = 0; idx < numPartitions2; ++idx) {
             console.log(rx_err);
         } else {
             console.log("partition: " + partition);
-            console.log(payload);
-            
-            var tmpstring = JSON.stringify(payload);
-            tmpstring = tmpstring.replace("}", ",\"time\":\"" + payload.timecreated + "\"}");
-            wss.broadcast(tmpstring);
+            //console.log(payload);
+		    var tmpstring;
+			//multiline
+			try {
+			
+                tmpstring = JSON.stringify(payload.split('\r\n'));
+				var jsonObj = JSON.parse( tmpstring  );
+		   
+		   	for (var item in jsonObj)
+					{
+					var targetstring =  JSON.stringify(jsonObj[item]);
+					targetstring=JSON.parse(targetstring);
+					
+				    targetstring = targetstring.replace("}", ",\"time\":\"" + JSON.parse(jsonObj[item]).timecreated + "\"}");
+					console.log("     sending : "+ targetstring);
+                    wss.broadcast(targetstring);
+				}
+			
+			    tmpstring="";
+				
+				//var lineseperatedarray = tmpstring.split("\r\n");
+				//console.log("lineseperatedarray is "+ lineseperatedarray.toString());
+				//for (var item in lineseperatedarray)
+				//	{
+				//	var targetstring =  JSON.parse('[' + lineseperatedarray[item]+ ']');
+				//	
+				//	targetstring = tmpstring.replace("}", ",\"time\":\"" + payload.timecreated + "\"}");
+				//	console.log("targetstring is "+ targetstring);
+                //    wss.broadcast(targetstring);
+				//	}
+			    //var jsonObj = JSON.parse('[' + tmpstring + ']');
+				//console.log("jsonObj is "+ jsonObj.toString());
+				//for (var item in jsonObj)
+				//	{
+				//	var targetstring =  JSON.stringify(jsonObj[item]);
+				//	targetstring = tmpstring.replace("}", ",\"time\":\"" + payload.timecreated + "\"}");
+                //    wss.broadcast(targetstring);
+				//	}
+			}
+			//single item
+			catch(err) {
+			try{
+					tmpstring = JSON.stringify(payload);
+				    tmpstring = tmpstring.replace("}", ",\"time\":\"" + payload.timecreated + "\"}");
+					console.log("    sending : "+ tmpstring);
+                    wss.broadcast(tmpstring);
+			}catch (err){
+			console.log(err.message);
+			}
+				
+					tmpstring="";
+			}
+
         }
     }
     );
